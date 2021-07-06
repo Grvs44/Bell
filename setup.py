d7f773e01896c7e490d9ab30fe9e99c32294667b
JoeGreaves17 <74414406+JoeGreaves17@users.noreply.github.com>
2021-07-06 19:50:48 +0100

    Added wheel and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name='Bell',
+    version='1.0.0',
+    description='A sample Python project',
+    url='http://grvs.co.uk',
+    author='Joe Greaves',
+    author_email='joe@grvs.co.uk',
+    license='MIT',
+    project_urls={'Homepage':'http://grvs.co.uk'},
+    packages=find_packages()
+)
